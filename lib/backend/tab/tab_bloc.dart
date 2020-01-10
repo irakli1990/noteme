0c697ab3ff9c2f892c4f218d9c44cdff242fc993
Irakli <iraklikardawa@gmail.com>
2020-01-10 12:58:18 +0100

    simple bloc delegate is created

--- lib/backend/tab/tab_bloc.dart
+++ lib/backend/tab/tab_bloc.dart
@@ -1,15 +1,17 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
-import './bloc.dart';
+import 'package:noteme/backend/tab/tab_event.dart';
+import 'package:noteme/models/tab.dart';
 
-class TabBloc extends Bloc<TabEvent, TabState> {
+
+class TabBloc extends Bloc<TabEvent, AppTab> {
   @override
-  TabState get initialState => InitialTabState();
+  AppTab get initialState => AppTab.task;
 
   @override
-  Stream<TabState> mapEventToState(
-    TabEvent event,
-  ) async* {
-    // TODO: Add Logic
+  Stream<AppTab> mapEventToState(TabEvent event) async* {
+    if (event is UpdateTab) {
+      yield event.tab;
+    }
   }
 }
--- lib/backend/tab/tab_event.dart
+++ lib/backend/tab/tab_event.dart
@@ -1,4 +1,18 @@
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:noteme/models/tab.dart';
 
-@immutable
-abstract class TabEvent {}
+abstract class TabEvent extends Equatable {
+  const TabEvent();
+}
+
+class UpdateTab extends TabEvent {
+  final AppTab tab;
+
+  const UpdateTab(this.tab);
+
+  @override
+  List<Object> get props => [tab];
+
+  @override
+  String toString() => 'UpdateTab { tab: $tab }';
+}
