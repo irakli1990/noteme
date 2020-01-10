63b327e9d74fd576321380e68f7f9a4fb06384cb
Irakli <iraklikardawa@gmail.com>
2020-01-10 13:18:32 +0100

    added database crud operations for note model

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:noteme/models/note.dart';
 import 'package:noteme/models/task.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
@@ -13,12 +14,17 @@ class DatabaseHelper {
   DatabaseHelper._createInstance();
 
   String taskID = 'id';
+  String noteID = 'id';
   String taskName = 'name';
+  String noteName = 'name';
   String taskAddTime = 'addTime';
+  String noteAddTime = 'addTime';
   String taskRememberTime = 'rememberTime';
   String taskPriority = 'priprity';
   String taskStatus = 'status';
+  String noteStatus = 'status';
   String taskTable = 'tasks';
+  String noteTable = 'notes';
 
   factory DatabaseHelper() {
     if (_databaseHelper == null) {
@@ -42,10 +48,15 @@ class DatabaseHelper {
   }
 
   void _createDB(Database db, int version) async {
+    // create tasks table
     await db.execute(
         'CREATE TABLE $taskTable($taskID INTIGER PRIMARY KEY AUTOINCREMENT,$taskName TEXT,$taskAddTime DATE,$taskRememberTime DATE,$taskPriority INTEGER, $taskStatus INTEGER)');
+    // create note table
+    await db.execute(
+        'CREATE TABLE $noteTable($noteID INTIGER PRIMARY KEY AUTOINCREMENT,$noteName TEXT,$noteAddTime DATE,$noteStatus INTEGER)');
   }
 
+// start tasks CRUD functions/
   Future<List<Map<String, dynamic>>> getTasks() async {
     Database database = await this.database;
     var result = await database
@@ -82,4 +93,46 @@ class DatabaseHelper {
     }
     return taskList;
   }
+
+  // end task CRUD functions
+  //---------------------------------------------------------------//
+  // start note CRUD functions
+
+  Future<List<Map<String, dynamic>>> getNotes() async {
+    Database database = await this.database;
+    var result = await database
+        .rawQuery('SELECT * FROM $noteTable ORDER BY $noteAddTime ASC');
+    return result;
+  }
+
+  Future<int> saveNote(Note note) async {
+    Database database = await this.database;
+    var result = await database.insert(noteTable, note.toJson());
+    return result;
+  }
+
+  Future<int> updateNote(Note note) async {
+    Database database = await this.database;
+    var result = await database.update(noteTable, note.toJson(),
+        where: '$noteID', whereArgs: [noteID]);
+    return result;
+  }
+
+  Future<int> deleteNote(Task task) async {
+    Database database = await this.database;
+    var result =
+        await database.delete(noteTable, where: '$noteID', whereArgs: [noteID]);
+    return result;
+  }
+
+  Future<List<Note>> getNoteList() async {
+    var noteListMap = await getNotes();
+    int count = noteListMap.length;
+    List<Note> noteList = List<Note>();
+    for (int i = 0; i < count; i++) {
+      noteList.add(Note.fromJson(noteListMap[i]));
+    }
+    return noteList;
+  }
+// end note CRUD functions
 }
